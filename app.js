671d6a14be32674144950ef5d3d06134b9e74b35
Gottardy <gottgab@gmail.com>
2023-01-30 18:51:56 -0500

    se modifica la app.js para que llame la clase server

---
+++ app.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+
+const port = process.env.PORT
+
+app.get('/', (req, res) =>{
+  res.send('Hello World')
+})
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+  })
