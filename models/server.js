e1bc879e45d479edb2651e8ea69bcca77c9ce9cd
Gottardy <gottgab@gmail.com>
2023-01-30 20:50:31 -0500

    modificamos el archivo server con los metodos de una api

--- models/server.js
+++ models/server.js
@@ -14,12 +14,33 @@ class Server {
   }
 
   middlewares() {
+    // directorio publico
     this.app.use(express.static('public'));
   }
 
   routes() {
-    this.app.get('/', (req, res) => {
-      res.send("Hello World");
+    this.app.get('/api', (req, res) => {
+      res.json({
+        msg: 'get API'
+      });
+    });
+
+    this.app.put('/api', (req, res) => {
+      res.json({
+        msg: 'put API'
+      });
+    });
+
+    this.app.post('/api', (req, res) => {
+      res.json({
+        msg: 'post API'
+      });
+    });
+
+    this.app.delete('/api', (req, res) => {
+      res.json({
+        msg: 'delete API'
+      });
     });
   }
 
