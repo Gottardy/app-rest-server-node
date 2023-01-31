b4b35737cdb258fc8de7e686630d04d7c3650643
Gottardy <gottgab@gmail.com>
2023-01-30 19:04:15 -0500

    actualisacion de los metodos que instancian el server

--- models/server.js
+++ models/server.js
@@ -1,9 +1,23 @@
-const express = require('express')
+const express = require("express");
 
-class Server{
-    constructor(){
-        this.app = express()
-    }
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+    this.routes();
+  }
+
+  routes() {
+    this.app.get("/", (req, res) => {
+      res.send("Hello World");
+    });
+  }
+
+  listener() {
+    this.app.listen(this.port, () => {
+      console.log(`App listening on port ${this.port}`);
+    });
+  }
 }
 
 module.exports = Server;
